c423020a51dfd305fddba5448e7930f7fccb3040
Vincent <vincent.prevosto at duke.edu>
2017-10-10 21:08:05 -0400

    conv TTL to trial

--- ConvTTLtoTrials.m
+++ ConvTTLtoTrials.m
@@ -16,19 +16,25 @@ if max(unique(TTLseq))< 1000 % stimulations
     Trials.start=Trials.TTLtimes;
     Trials.end=Trials.start+TTLdur;
     Trials.interval=Trials.start(2:end)-Trials.end(1:end-1);
-else % task trials
-    % In behavioral recordings, task starts with double TTL (e.g., two 10ms
-    % TTLs, with 10ms interval). These pulses are sent at the begining of
-    % each trial(e.g.,head through front panel). One pulse is sent at the
-    % end of each trial. With sampling rate of 30kHz, that interval should
-    % be 601 samples (20ms*30+1). Or 602 accounting for jitter.
-    % onTTL_seq at native sampling rate should thus read as:
-    %   601
-    %   end of trial time
-    %   inter-trial interval
-    %   601 ... etc
+elseif  max(unique(TTLseq))/TTLtimes>0.9 % video sync at beginning and end of recording 
     
-    %NEW CODE, UNTESTED
+    Trials.start=Trials.TTLtimes;
+    Trials.end=Trials.start+TTLdur;
+    Trials.interval=Trials.start(2:end)-Trials.end(1:end-1);
+else % task trials
+        
+%     % In behavioral recordings, task starts with double TTL (e.g., two 10ms
+%     % TTLs, with 10ms interval). These pulses are sent at the begining of
+%     % each trial(e.g.,head through front panel). One pulse is sent at the
+%     % end of each trial. With sampling rate of 30kHz, that interval should
+%     % be 601 samples (20ms*30+1). Or 602 accounting for jitter.
+%     % onTTL_seq at native sampling rate should thus read as:
+%     %   601
+%     %   end of trial time
+%     %   inter-trial interval
+%     %   601 ... etc
+%     
+%     %NEW CODE, UNTESTED
 %     TTLlength=mode(TTLdur); %in ms
 %     
 %     if TTLseq(1)>=TTLlength*2+10 %missed first trial initiation, discard times
