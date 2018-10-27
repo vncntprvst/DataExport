5179cdf3363a489887cbbee6fb1eb9efb9aa71ca
Vincent Prevosto <vincent.prevosto@duke.edu>
2018-10-27 16:58:31 -0400

    improved geom for KS export

--- DataExportGui.m
+++ DataExportGui.m
@@ -1338,34 +1338,42 @@ end
 if get(handles.CB_KSChannelMap,'value')==1
     probeInfo.numChannels=length(handles.rec_info.exportedChan);
     if isfield(handles.rec_info,'probeLayout')
+        switch handles.rec_info.sys
+            case 'OpenEphys'
+                probeInfo.chanMap=[handles.rec_info.probeLayout.OEChannel];
+            case 'Blackrock'
+                probeInfo.chanMap=[handles.rec_info.probeLayout.BlackrockChannel];
+        end
         if isfield(handles,'remapped') && handles.remapped==true
-            probeInfo.chanMap=1:probeInfo.numChannels;
-        else
-            switch handles.rec_info.sys
-                case 'OpenEphys'
-                    probeInfo.chanMap=[handles.rec_info.probeLayout.OEChannel];
-                case 'Blackrock'
-                    probeInfo.chanMap=[handles.rec_info.probeLayout.BlackrockChannel];
-            end
+            [~,chSortIdx]=sort(probeInfo.chanMap);
+            handles.rec_info.probeLayout=handles.rec_info.probeLayout(chSortIdx);
+            probeInfo.chanMap=probeInfo.chanMap(chSortIdx);
+        else       
+            probeInfo.chanMap=1:probeInfo.numChannels;  
         end
         probeInfo.connected=true(probeInfo.numChannels,1);
         probeInfo.connected(isnan([handles.rec_info.probeLayout.Shank]))=0;
         probeInfo.kcoords=[handles.rec_info.probeLayout.Shank];
         probeInfo.kcoords=probeInfo.kcoords(~isnan([handles.rec_info.probeLayout.Shank]));
-        probeInfo.xcoords = zeros(1,probeInfo.numChannels);
-        probeInfo.ycoords = 200 * ones(1,probeInfo.numChannels);
-        groups=unique(probeInfo.kcoords);
-        for elGroup=1:length(groups) 
-            if isnan(groups(elGroup))
-                continue;
+        if isfield(handles.rec_info.probeLayout,'x_geom')
+            probeInfo.xcoords = [handles.rec_info.probeLayout.x_geom];
+            probeInfo.ycoords = [handles.rec_info.probeLayout.y_geom];
+        else
+            probeInfo.xcoords = zeros(1,probeInfo.numChannels);
+            probeInfo.ycoords = 200 * ones(1,probeInfo.numChannels);            
+            groups=unique(probeInfo.kcoords);
+            for elGroup=1:length(groups)
+                if isnan(groups(elGroup))
+                    continue;
+                end
+                groupIdx=find(probeInfo.kcoords==groups(elGroup));
+                probeInfo.xcoords(groupIdx(2:2:end))=20;
+                probeInfo.xcoords(groupIdx)=probeInfo.xcoords(groupIdx)+(0:length(groupIdx)-1);
+                probeInfo.ycoords(groupIdx)=...
+                    probeInfo.ycoords(groupIdx)*(elGroup-1);
+                probeInfo.ycoords(groupIdx(round(end/2)+1:end))=...
+                    probeInfo.ycoords(groupIdx(round(end/2)+1:end))+20;
             end
-            groupIdx=find(probeInfo.kcoords==groups(elGroup));
-            probeInfo.xcoords(groupIdx(2:2:end))=20;
-            probeInfo.xcoords(groupIdx)=probeInfo.xcoords(groupIdx)+(0:length(groupIdx)-1);
-            probeInfo.ycoords(groupIdx)=...
-            probeInfo.ycoords(groupIdx)*(elGroup-1);
-            probeInfo.ycoords(groupIdx(round(end/2)+1:end))=...
-                probeInfo.ycoords(groupIdx(round(end/2)+1:end))+20;
         end
     end
     
